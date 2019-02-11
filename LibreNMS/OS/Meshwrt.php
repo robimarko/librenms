785c2c884f026f5c2814e26049a9f07b68ca3300
Robert Marko <robimarko@gmail.com>
2019-02-11 19:10:02 +0100

    Initial wireless sensors

---
+++ LibreNMS/OS/Meshwrt.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Meshwrt.php
+ *
+ * Cilab MeshWrt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 Robert Marko
+ * @author     Robert Marko <robimarko@gmail.com>
+ */
+
+namespace LibreNMS\OS;
+
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessCapacityDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessCcqDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessClientsDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessDistanceDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessFrequencyDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessNoiseFloorDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessQualityDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRateDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\OS;
+
+class Meshwrt extends OS implements
+    WirelessCapacityDiscovery,
+    WirelessCcqDiscovery,
+    WirelessClientsDiscovery,
+    WirelessDistanceDiscovery,
+    WirelessFrequencyDiscovery,
+    WirelessNoiseFloorDiscovery,
+    WirelessPowerDiscovery,
+    WirelessQualityDiscovery,
+    WirelessRateDiscovery,
+    WirelessRssiDiscovery
+
+{
+	/**
+	 * Discover wireless client counts. Type is clients.
+	 * Returns an array of LibreNMS\Device\Sensor objects that have been discovered
+	 *
+	 * @return array Sensors
+	 */
+	public function discoverWirelessClients()
+	{
+	    $oid = '.1.3.6.1.4.1.51510.11.1.101.1';
+	    return array(
+	        new WirelessSensor('clients', $this->getDeviceId(), $oid, 'meshwrt', 1, 'Clients'),
+	    );
+	}
+}
