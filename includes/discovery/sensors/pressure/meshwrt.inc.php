5c4306006966e61f52fd7b147346f7725c906d47
Robert Marko <robimarko@gmail.com>
2019-02-12 18:42:50 +0100

    Add BME280 sensor data

---
+++ includes/discovery/sensors/pressure/meshwrt.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * meshwrt.inc.php
+ *
+ * LibreNMS pressure discovery module for Crisis Innovation Lab MeshPoint
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 Robert Marko
+ * @author     Robert Marko <robimarko@gmail.com>
+ */
+
+$oid = '.1.3.6.1.4.1.51510.14.2.101.1';
+$current = (snmp_get($device, $oid, '-Oqv') / $divisor);
+discover_sensor($valid['sensor'], 'pressure', $device, $oid, 1, 'meshwrt', 'Air pressure', 1, 1, null, null, null, null, $current);
