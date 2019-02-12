f7058f839387b9ef0b529ddc44b32d7fa182c6fe
Robert Marko <robimarko@gmail.com>
2019-02-12 13:11:49 +0100

    7

--- LibreNMS/OS/Meshwrt.php
+++ LibreNMS/OS/Meshwrt.php
@@ -58,12 +58,14 @@ class Meshwrt extends OS implements
 	public function discoverWirelessPower()
 	{
 	    $tx_oid = '.1.3.6.1.4.1.51510.11.2.101.1';
-	    $tx2_oid = '.1.3.6.1.4.1.51510.11.2.101.1';
+	    $tx2_oid = '.1.3.6.1.4.1.51510.11.2.101.2';
 	    $rx_oid = '.1.3.6.1.4.1.51510.11.3.101.1';
+	    $rx2_oid = '.1.3.6.1.4.1.51510.11.3.101.1';
 	    return array(
-	        new WirelessSensor('power', $this->getDeviceId(), $tx_oid, 'meshwrt-tx', 1, 'Tx Power'),
-	        new WirelessSensor('power', $this->getDeviceId(), $tx2_oid, 'meshwrt-tx-2', 1, 'Tx Power'),
-	        new WirelessSensor('power', $this->getDeviceId(), $rx_oid, 'meshwrt-rx', 1, 'Signal Level'),
+	        new WirelessSensor('power', $this->getDeviceId(), $tx_oid, 'meshwrt-tx', 1, '2.4 GHz Tx Power'),
+	        new WirelessSensor('power', $this->getDeviceId(), $tx2_oid, 'meshwrt-tx-2', 1, '5 GHz Tx Power'),
+	        new WirelessSensor('power', $this->getDeviceId(), $rx_oid, 'meshwrt-rx', 1, '2.4 GHzSignal Level'),
+	        new WirelessSensor('power', $this->getDeviceId(), $rx2_oid, 'meshwrt-rx-2', 1, '5 GHz Signal Level'),
 	    );
 	}
 }
