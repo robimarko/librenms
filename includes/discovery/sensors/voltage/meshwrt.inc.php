c75bb655727cb00330d344f16076bf136142bca6
Robert Marko <robimarko@gmail.com>
2019-03-19 22:14:43 +0100

    Change voltage limits

--- includes/discovery/sensors/voltage/meshwrt.inc.php
+++ includes/discovery/sensors/voltage/meshwrt.inc.php
@@ -26,11 +26,11 @@
 $oid = '.1.3.6.1.4.1.51510.12.1.101.1';
 $current = (snmp_get($device, $oid, '-Oqv') / $divisor);
 if ($current > 0) {
-	discover_sensor($valid['sensor'], 'voltage', $device, $oid, 1, 'meshwrt', 'Input voltage', 1000, 1, 9, 8, 26, 28, $current);
+	discover_sensor($valid['sensor'], 'voltage', $device, $oid, 1, 'meshwrt', 'Input voltage', 1000, 1, 10.5, 9, 26, 28, $current);
 }
 
 $oid = '.1.3.6.1.4.1.51510.13.1.101.1';
 $current = (snmp_get($device, $oid, '-Oqv') / $divisor);
 if ($current > 0) {
-	discover_sensor($valid['sensor'], 'voltage', $device, $oid, 2, 'meshwrt', ' POE Output voltage', 1000, 1, 9, 8, 26, 28, $current);
+	discover_sensor($valid['sensor'], 'voltage', $device, $oid, 2, 'meshwrt', ' POE Output voltage', 1000, 1, 10.5, 9, 26, 28, $current);
 }
